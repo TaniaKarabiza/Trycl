6486d4dab50891ee00fbc22f43c58124fa2c81a9
tania <taniakarabiza@gmail.com>
2022-04-08 22:48:30 -0400

    commit

--- src/test/java/com/cydeo/step_definitions/deleteFileOrFolder.java
+++ src/test/java/com/cydeo/step_definitions/deleteFileOrFolder.java
@@ -58,7 +58,8 @@ public class deleteFileOrFolder {
         //Assert.assertTrue(deletedFilesPage.locatingAnyDeletedFile("Tasks").isDisplayed());
 
         System.out.println(textOfFile);
-       // deletedFilePage.locatingAnyDeletedFile(textOfFile);
+      // deletedFilePage.locatingAnyDeletedFile(textOfFile);
+
 
         List<WebElement> eachText=deletedFilePage.eachDeleted;
         for (WebElement each : eachText) {
@@ -67,6 +68,8 @@ public class deleteFileOrFolder {
 
             }
         }
+
+
 
 
 
